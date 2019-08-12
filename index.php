069e29abd29d1c9cc8f91bcc422c96b0687c2eeb
megha-gith <meghapatil1408@gmail.com>
2019-08-12 18:47:52 +0530

    justsend

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE Html>
+	<html>
+	<head>
+		<title>sender page</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="index.css">
+	</head>
+	<body>
+			<div class="main-container">
+				
+				<div class="box">
+					<div class="head">
+						<h1>JustSend</h1>
+					</div>
+					<div class="input-box">
+						<a href="sender.php" class="button">START A MESSAGE</a>
+					</div>
+					<div class="input-box">
+						<a href="receiver.php" class="button">READ A MESSAGE</a>
+					</div>
+					<div>
+						<p><b><h3 style="font-weight: 600;">You send an attachment in a text message
+				</div>	
+					
+			</div>
+	
+	</body>
+	</html>
